497226921c3abd0e35c597de5e4a8e1c1c5c09a6
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-07-12 12:01:24 -0400

    added notes to outline thoughts and procedures

--- examples/single-blog-post/main.js
+++ examples/single-blog-post/main.js
@@ -1,3 +1,5 @@
+// =======Given Code=======
+
 const blogPost = {
   title: "The Amazing Sloth",
   meta: {
@@ -13,4 +15,44 @@ const blogPost = {
   }
 };
 
-// Start Editing Here
+// ===================THE LONG WAY===========================
+
+// Don't forget to access the area that you are manipulating
+//
+// let sloths = document.querySelector(.container);
+//
+// // Webpage includes header, image, meta, post, and site
+// // (all pulled from the CSS)
+//
+// // -------all of the content of the article------
+// let content = document.createElement('article');
+//
+// // -------------the header------------------------
+// let header = document.createElement('h2');
+// let headertext = document.createTextNode('The Amazing Sloth');
+//
+// // --------------------the body----------------------
+// let meta = document.createElement('date');
+//
+// let headerimage = document.setAttribute('src', image);
+//
+// let post = document.createElement()
+// let post = document.createTextNode('Sloths make very good habitats for other organisms, and a single sloth may be home to several species of moths, beetles, cockroaches, fungi, ciliates, and algae');
+//
+// // ---------------------the footer-------------------
+// let site = document.createElement()
+//
+//
+// +++++++AND THEN YOU WOULD APPEND THE CHILDREN+++++++
+
+// ================= Using Template Literals ====================
+
+// create a template that can be manipulated by changing
+// the values of the keys in the const blogPost
+let template = `
+
+<h2>${title}</h2>
+<div>${meta}</div>
+<div>${post}</div>
+<div>${site}</div>
+`
